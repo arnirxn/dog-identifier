5699dbad35c7083bece444ebdecf282405d87e64
arnirxn <arnfried.rixen@gmail.com>
2022-11-03 23:00:36 +0100

    Add docstrings and fix imports

--- extract_bottleneck_features.py
+++ extract_bottleneck_features.py
@@ -1,27 +1,23 @@
-""" """
+"""Utility to make predictions on tensors using Keras Applications."""
 
-from keras.applications.inception_v3 import InceptionV3, preprocess_input
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-from keras.applications.vgg16 import VGG16, preprocess_input
-from keras.applications.vgg19 import VGG19, preprocess_input
-from keras.applications.xception import Xception, preprocess_input
+from tensorflow.keras.applications import inception_v3, resnet50, vgg16, vgg19, xception
 
 
 def extract_VGG16(tensor):
-    return VGG16(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+    return vgg16.VGG16(weights='imagenet', include_top=False).predict(vgg16.preprocess_input(tensor))
 
 
 def extract_VGG19(tensor):
-    return VGG19(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+    return vgg19.VGG19(weights='imagenet', include_top=False).predict(vgg19.preprocess_input(tensor))
 
 
 def extract_Resnet50(tensor):
-    return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+    return resnet50.ResNet50(weights='imagenet', include_top=False).predict(resnet50.preprocess_input(tensor))
 
 
 def extract_Xception(tensor):
-    return Xception(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+    return xception.Xception(weights='imagenet', include_top=False).predict(xception.preprocess_input(tensor))
 
 
 def extract_InceptionV3(tensor):
-    return InceptionV3(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+    return inception_v3.InceptionV3(weights='imagenet', include_top=False).predict(inception_v3.preprocess_input(tensor))
